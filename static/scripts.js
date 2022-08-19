b8b6375658cad57e0b3f88b483d1178f2f622146
sealvia <silviafontfreda@gmail.com>
2022-08-19 03:03:38 +0200

    New switch style icon

--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,23 @@
 function toggleStyle() {
 
-    var element = document.getElementById("main");
-
+    let element = document.getElementById("main");
+    let icon = document.getElementById("style-switcher");
+    
     if (element.classList.contains("light")) {
 
         element.classList.add("dark");
         element.classList.remove("light")
+
+        icon.src = "static/media/sun.png"
     } 
     else if (element.classList.contains("dark")) {
 
         element.classList.add("light");
         element.classList.remove("dark")
+
+        icon.src = "static/media/moon.png"
+
     }
+
+
 }
